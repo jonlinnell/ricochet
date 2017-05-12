86c47389c39b18f7b0ddc19e00d934b7a47b1994
Jon Linnell <jon@london-staff-2-59.lut.ac.uk>
2017-05-12 10:15:54 +0100

    Fixed formatting. Added linting.

--- index.js
+++ index.js
@@ -4,40 +4,40 @@ var morgan = require('morgan');
 var fs = require('fs');
 var path = require('path');
 var rfs = require('rotating-file-stream');
-var colours = require('colors');
-var _ = require('lodash');
+var colours = require('colors'); // eslint-disable-line no-unused-vars
 
 var app = express();
 
-const logDirectory = path.join(__dirname, 'log')
-const port = process.env.PORT || 9000;
 const config = require('./config');
 
-morgan.token('real-ip', function (req, res) { return req.headers['x-real-ip'] });
+const logDirectory = path.join(__dirname, 'log');
+const port = process.env.PORT || 9000;
 
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
+morgan.token('real-ip', (req, res) => { return req.headers['x-real-ip']; }); // eslint-disable-line no-unused-vars
+
 var accessLogStream = rfs('access.log', {
-  interval: '1d',
+  interval: config.logInterval,
+  compress: 'gzip',
   path: logDirectory
 });
 
-app.use(morgan('[:date[web]] ' + _.padEnd('(:real-ip)', 15) + ':url'.green));
+app.use(morgan('[:date[web]] ' + ':real-ip'.blue + '\t:url'.green));
 app.use(morgan(':real-ip - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"', {stream: accessLogStream}));
 
-// Load routes
-for (let [key, value] of Object.entries(config.redirects)) {  
-  app.get('/'+key, function(req, res) {
+// Load redirects from config
+for (let [key, value] of Object.entries(config.redirects)) {
+  app.get('/' + key, (req, res) => {
     res.redirect(value);
-    console.log(JSON.stringify(req.headers,null,2));
   });
 }
 
-// Send everything else to the homepage
-app.get('*', function(req, res) {
+// Default redirect for everything else
+app.get('*', (req, res) => {
   res.redirect(config.defaultRedirect);
 });
 
-http.createServer(app).listen(port, function() {
-  console.log('Server listening on port ' + port);
+http.createServer(app).listen(port, () => {
+  console.log('Server listening on port ' + port); // eslint-disable-line no-console
 });
