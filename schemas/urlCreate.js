ba4af86ab64a4f86114797c7004f4fa90448bcee
Jon Linnell <linnell.jon@gmail.com>
2017-11-29 19:43:52 +0000

    Allowing submission of shortlinks including /

--- schemas/urlCreate.js
+++ schemas/urlCreate.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 
 module.exports = Joi.object().keys({
   title: Joi.string()
-    .regex(/^[A-Za-z0-9_-]{2,256}$/)
+    .regex(/^(?!\/)[A-Za-z0-9\/_-]{2,256}$/) // eslint-disable-line no-useless-escape
     .regex(/^url$/i, { invert: true })
     .regex(/^auth$/i, { invert: true })
     .required(),
