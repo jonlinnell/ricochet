1bdab1c17da9035f71591f36d04d67cbb345979b
Jon Linnell <linnell.jon@gmail.com>
2017-11-29 18:16:47 +0000

    UUID validation

--- routes/redirects.js
+++ routes/redirects.js
@@ -8,6 +8,7 @@ module.exports = (app) => {
     .then((urls) => {
       urls.forEach((thisUrl) => {
         app.get(`/${thisUrl.title}`, cookieAgent, (req, res) => {
+          console.log(req.rtuid)
           Click.create({
             userId: req.rtuid,
             URLId: thisUrl.id
