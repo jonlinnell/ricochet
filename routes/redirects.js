4c08bc9f9fac6b0b06d4f380261084d7c898bdcb
Jon Linnell <linnell.jon@gmail.com>
2019-02-27 14:33:38 +0000

    '/' redirects to DEFAULT_URL or 404s (for now)

--- routes/redirects.js
+++ routes/redirects.js
@@ -1,6 +1,16 @@
 const { URL, Click } = require('../models')
 
+const { DEFAULT_URL } = process.env
+
 module.exports = (redirector) => {
+  redirector.get('/', (req, res) => {
+    if (!DEFAULT_URL) {
+      res.sendStatus(404)
+    } else {
+      res.redirect(DEFAULT_URL)
+    }
+  })
+
   redirector.get('/:title', (req, res) => {
     URL.findOne({
       where: {
@@ -13,6 +23,9 @@ module.exports = (redirector) => {
         })
           .then(() => res.redirect(thisUrl.url))
           .catch(error => res.status(500).send(error)))
-      .catch(() => res.sendStatus(404))
+      .catch(() => {
+        res.sendStatus(404)
+        // @TODO: Show 'not found' page!
+      })
   })
 }
