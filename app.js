17eb77f42207e35789e88cdbbf7b241399798aa3
Jon Linnell <linnell.jon@gmail.com>
2019-04-05 10:43:32 +0100

    Use same server for API and links. Enforce HTTPS in prod

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ const routeClicks = require('./routes/clicks')
 const routeRedirects = require('./routes/redirects')
 const routeUrl = require('./routes/url')
 
-const redirector = express()
 const app = express()
 
-const apiPort = process.env.API_PORT || 9000
-const port = process.env.PORT || 3000
+const {
+  NODE_ENV, PORT, CREATE_DEFAULT_ADMIN, DEFAULT_ADMIN_PASSWORD, SSL_CERT, SSL_KEY
+} = process.env
 
 const logDir = path.join(__dirname, 'logs')
 if (!fs.existsSync(logDir)) {
@@ -43,7 +43,7 @@ app.use(cors())
 app.use(helmet())
 app.use(morgan('combined', { stream: accessLog }))
 
-console.log(`Launching in ${process.env.NODE_ENV || 'development'} mode.`.cyan)
+console.log(`Launching in ${NODE_ENV || 'development'} mode.`.cyan)
 
 /* Remember to filter fixed routes in the Joi schema */
 app.use('/auth', routeAuth)
@@ -52,17 +52,19 @@ app.use('/url', routeUrl)
 
 app.get('/favicon.ico', (req, res) => res.send(204))
 
+app.use('/', routeRedirects)
+
 models.sequelize.sync().then(() => {
   console.log(`[${'DB'.bold}] Connection established.`.green)
 
-  if (process.env.CREATE_DEFAULT_ADMIN) {
+  if (CREATE_DEFAULT_ADMIN) {
     User.findOne({
       where: { username: 'admin' },
       attributes: { exclude: ['password'] }
     })
       .then((user) => {
         if (!user) {
-          const hashedPassword = bcrypt.hashSync(process.env.DEFAULT_ADMIN_PASSWORD || 'not @ password', 8)
+          const hashedPassword = bcrypt.hashSync(DEFAULT_ADMIN_PASSWORD || 'not @ password', 8)
 
           User.create({
             username: 'admin',
@@ -75,24 +77,15 @@ models.sequelize.sync().then(() => {
       })
   }
 
-  app.get('/status', (req, res) => {
-    res.status(200).json({ message: 'Hello there!' })
-  })
-
-  redirector.use('/', routeRedirects)
-
-  if (process.env.NODE_ENV === 'production') {
+  if (NODE_ENV === 'production') {
     const options = {
-      cert: fs.readFileSync(process.env.SSL_CERT),
-      key: fs.readFileSync(process.env.SSL_KEY)
+      cert: fs.readFileSync(SSL_CERT),
+      key: fs.readFileSync(SSL_KEY)
     }
-    https.createServer(options, app).listen(apiPort)
-    console.log(`[${'API'.bold}] Service (https) started on ${apiPort}.`.green)
+    https.createServer(options, app).listen(PORT)
+    console.log(`Service (https) started on ${PORT}.`.green)
   } else {
-    http.createServer(app).listen(apiPort)
-    console.log(`[${'API'.bold}] Service (http) started on ${apiPort}.`.green)
+    http.createServer(app).listen(PORT)
+    console.log(`Service (http) started on ${PORT}.`.green)
   }
-
-  http.createServer(redirector).listen(port)
-  console.log(`[${'WEB'.bold}] Service started on ${port}.`.green)
 })
--- schemas/urlCreate.js
+++ schemas/urlCreate.js
@@ -3,11 +3,12 @@ const Joi = require('joi')
 module.exports = Joi.object().keys({
   title: Joi.string()
     .regex(/^(?!\/)[A-Za-z0-9\/_-]{2,256}$/) // eslint-disable-line no-useless-escape
+    .regex(/^favicon\.ico$/i, { invert: true })
     .regex(/^url$/i, { invert: true })
     .regex(/^auth$/i, { invert: true })
     .regex(/^clicks$/i, { invert: true })
     .required(),
   url: Joi.string()
-    .regex(/^http(s)?:\/\/[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/)
+    .regex(/^https:\/\/[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/)
     .required()
 })
