10c8b42637e0a62ee9016bb0e87e9ca2cd85b186
Jon Linnell <linnell.jon@gmail.com>
2019-04-05 09:16:07 +0100

    Use moment-tz for dates. Fix yarn.lock.

--- client/src/lib/dates.js
+++
@@ -1,6 +0,0 @@
-export const zeroPad = n => (n < 10 ? `0${n}` : n)
-
-export const formatDate = (rawDate) => {
-  const date = new Date(rawDate)
-  return `${date.getFullYear()}-${zeroPad(date.getMonth() + 1)}-${zeroPad(date.getDate())}`
-}
